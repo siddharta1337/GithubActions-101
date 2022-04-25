f80aad82aabcf31e1a62de289a572d0bad3c929b
carlos solis <carlos.solis.d@gmail.com>
2022-04-24 23:52:09 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+class User {
+    constructor() {
+        this.message = 'Hello World'
+        console.log(this.message);
+    }
+
+    getUserName(){}
+
+    getUserStatus(){}
+  
+  
+  }
+  
+  module.exports = User;
